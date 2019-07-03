1c931caaaf9cbf6ab8e4152da1936caefec7579b
Danny Lee <dreamer@panix.com>
2019-07-03 21:17:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def roll_call_dwarves(dwarfs)
   namehash = []
-      
+binding.pry      
   dwarfs.each_with_index { |name, index| 
     namehash[index] = puts "#{index+1}. #{name}" 
     }
